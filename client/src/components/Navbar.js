269fad7563cd2e45db4e69a99bddf43f6d82cff6
fareeboy12 <fareeboy12@gmail.com>
2022-06-19 16:51:13 +0500

    Search functionality Started

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -20,6 +20,7 @@ const Navbar = () => {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
   const [mainSidebar, setMainSidebar] = useState(null)
+  const [search, setSearch] = useState("")
   const handleSidebar = () => {
     if (isUserAuthenticated() && isCreator()) {
       setMainSidebar(creatorSidebar)
@@ -33,7 +34,9 @@ const Navbar = () => {
     handleSidebar()
   }, [isUserAuthenticated(), isCreator()])
 
-
+  const handleSearch = (value) => {
+    setSearch(value)
+  }
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
@@ -50,7 +53,7 @@ const Navbar = () => {
             <Col xs='4' lg='8'>
               <h6 className='color-white py-3 px-4 border border-1 border-light radius_15 w-50 mx-auto d-flex align-items-center'>
                 <BsSearch className='me-3' />
-                <input type="text" className='bg-transparent border-0 w-100 color-white outline_none' placeholder='Search' />
+                <input type="text" value={search} onChange={(e)=>(handleSearch(e.target.value))} className='bg-transparent border-0 w-100 color-white outline_none' placeholder='Search' />
               </h6>
             </Col>
             <Col xs='4' lg='2' className='d-flex align-items-center justify-content-end'>
