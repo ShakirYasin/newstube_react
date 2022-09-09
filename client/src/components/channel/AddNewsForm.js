cadf6dc3cd23a26fedadc05dc4306f60c0ccb9f0
ShakirYasin <shakir.yasin18@gmail.com>
2022-09-10 02:48:26 +0500

    Sudden Commit

--- client/src/components/channel/AddNewsForm.js
+++ client/src/components/channel/AddNewsForm.js
@@ -1,7 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { Alert, Button, Form } from 'react-bootstrap'
+import React, { useState, useContext, useEffect, useRef } from 'react'
+import { Alert, Button, Form, Spinner } from 'react-bootstrap'
 import axios from '../axios'
 // import FileInput from '../FileInput'
+import Tag from '../Tag'
+import {FiCheck} from 'react-icons/fi'
 
 import UserContext from '../../context/UserContext'
 import NewsContext from '../../context/NewsContext'
@@ -10,6 +12,7 @@ import { useFileUpload } from '../../hooks/useFileUpload'
 const NEWS_URL = '/posts'
 const AddNewsForm = () => {
 
+    const tagsRef = useRef(null)
     const { upload } = useFileUpload()
     const {auth} = useContext(UserContext)
     // const {setNewUserPost} = useContext(NewsContext)
@@ -17,9 +20,13 @@ const AddNewsForm = () => {
         title: "",
         description: "",
         image: null,
+        tags: [],
+        categories: []
     });
+    const [tempTag, setTempTag] = useState("")
     const [successMsg, setSuccessMsg] = useState('')
     const [errMsg, setErrMsg] = useState('')
+    const [uploadState, setUploadState] = useState(null)
 
     const handleChange = (name, value) => {
         setData(prev => (
@@ -30,9 +37,48 @@ const AddNewsForm = () => {
         ));
     };
 
-    // const handleInputState = (name, value) => {
-    //     setData((prev) => ({ ...prev, [name]: value }));
-    // };
+    const handleTag = (key) => {
+        if(key === "Enter"){
+            if(tempTag.match(/^\w+$/)){
+                setData(prev => (
+                    {
+                        ...prev,
+                        tags: [...prev.tags, tempTag]
+                    }
+                ))
+                setTempTag("")
+            }
+            else {
+                alert("Please only add Letters and Numbers")
+                setTempTag("")
+            }
+        }
+        if(key === "Backspace"){
+            if(tempTag === ""){
+                setData(prev => (
+                    {
+                        ...prev,
+                        tags: prev.tags.splice(0, prev.tags.length - 1)
+                    }
+                ))
+            }
+        }
+    }
+
+    const handleRemoveTag = (tag) => {
+        setData(prev => (
+            {
+                ...prev,
+                tags: prev.tags.filter(t => t !== tag)
+            }
+        ))
+    }
+    const formHandling = (e) => {
+        if(e.key === "Enter"){
+            e.preventDefault();
+            return false
+        }
+    }
 
 
     async function handleSubmit(e){
@@ -85,15 +131,16 @@ const AddNewsForm = () => {
     }
 
     const handleUpload = (file) => {
-        upload(file, "images", "image", setData)
+        upload(file, "images", "image", setData, setUploadState)
     }
 
     useEffect(() => {
-        console.log(data)
-    }, [data])
+        console.log(typeof uploadState)
+    }, [uploadState])
+
 
     return (
-        <Form onSubmit={(e) => (handleSubmit(e))}>
+        <Form onSubmit={(e) => (handleSubmit(e))} onKeyDown={(e) => (formHandling(e))}>
             {
                 successMsg &&
                 <Alert key='success' variant='success'>
@@ -135,7 +182,37 @@ const AddNewsForm = () => {
                                     ({...prev, image: e.target.files[0]}))
                             )}
                     />
-                    <Button type="button" onClick={() => (handleUpload(data?.image))} >Upload</Button>
+                    <Button type="button" className='btn_primary d-flex' disabled={uploadState === 100} style={{padding: "0.45rem 1rem"}} onClick={() => (handleUpload(data?.image))}>
+                        Upload
+                        <span className='ms-2'>
+                            {
+                                uploadState === null ?
+                                <></>
+                                :
+                                uploadState == 0 ?
+                                <Spinner size={20} color="white" />
+                                :
+                                uploadState == 100 &&
+                                <FiCheck size={20} color="white" />
+                            }
+                        </span>
+                    </Button>
+                </div>
+            </Form.Group>
+            
+            <Form.Group className="my-3">
+                <Form.Label>Tags (optional)</Form.Label>
+                <div className='border border-1 d-flex align-items-center flex-wrap w-50 bg-white p-2 radius_15' style={{cursor: "text"}} onClick={() => (tagsRef.current.focus())}>
+                    <div className='flex-shrink-0 d-flex align-items-center gap-2 flex-wrap' style={{width: "max-content", maxWidth: "100%"}}>
+                        {
+                            data?.tags?.map(tag => (
+                                <Tag tag={tag} handleRemoveTag={handleRemoveTag} />
+                            ))
+                        }
+                    </div>
+                    <div className=''>
+                        <Form.Control ref={tagsRef} value={tempTag} onChange={(e) => (setTempTag(e.target.value))} onKeyDown={(e) => (handleTag(e.key))} className="border-0 rounded-0 nostyle-input" />
+                    </div>
                 </div>
             </Form.Group>
             <Button type='submit' className='btn_primary mt-4'>Submit</Button>
